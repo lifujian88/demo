b58ff2325b95ccdef25ebe8a014301db6b342b9d
lifujian <lifujian@cn.ibm.com>
2019-10-17 14:20:54 +0800

    helloworld2

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld{
+
+        public static void main(String[] arr){
+            System.out.print(" 1");
+            System.out.print(" HELLO WORLD");
+            System.out.print(" 2");
+            System.out.print(" HELLO WORLD");
+        }
+
+    }
